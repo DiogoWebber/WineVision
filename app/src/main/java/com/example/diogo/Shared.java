660632664deece66e6519211ed246be17acc2dbe
DiogoWebber <diogoevaldt17@gmail.com>
2024-10-28 14:14:36 -0300

    fix

--- app/src/main/java/com/example/diogo/Shared.java
+++
@@ -1,9 +0,0 @@
-package com.example.diogo;
-
-public class Shared {
-    public static final String KEY_COR_RESERVA = "KEY_COR_RESERVADA";
-
-    public static final String KEY_COR_DISPONIVEL = "KEY_COR_DISPONIVEL";
-
-
-}
